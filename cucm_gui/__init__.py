c08c25e2e8039c603b17ffb2feb7626269927092
Joe Jacobs <joej@ignw.io>
2020-04-01 14:16:22 -0700

    adding bootstrap css to the app

--- cucm_gui/__init__.py
+++ cucm_gui/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from cucm_gui.config import Config
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
 
 from cucm_gui import routes
